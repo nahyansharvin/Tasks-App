472ff4c44de787b8308b9b2143a890a9c0605352
Nahyan Sharvin <nskonnalath@gmail.com>
2023-10-20 04:48:47 +0530

    feat: improve error handling

--- server/server.js
+++ server/server.js
@@ -10,33 +10,40 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb'}));
 
+// Middleware to handle errors
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({ message: 'Internal server error' });
+});
+
 
 // APIS
 // get all tasks
-app.get('/tasks', async (req, res) => {
+app.get('/tasks', async (req, res, next) => {
   try {
     const results = await query('SELECT * FROM tasks ORDER BY created_at DESC');
     res.json(results);
   } catch (error) {
-    console.error('Error fetching tasks:', error);
-    res.status(500).json({ message: 'Internal server error' });
+    next(error);
   }
 });
 
 // get task by id
-app.get('/tasks/:id', async (req, res) => {
+app.get('/tasks/:id', async (req, res, next) => {
   const id = req.params.id;
   try {
     const results = await query('SELECT * FROM tasks WHERE id = ?', [id]);
+    if (results.length === 0) {
+      return res.status(404).json({ message: 'Task not found' });
+    }
     res.json(results);
   } catch (error) {
-    console.error('Error fetching task:', error);
-    res.status(500).json({ message: 'Internal server error' });
+    next(error);
   }
 });
 
 // create task
-app.post('/tasks', async (req, res) => {
+app.post('/tasks', async (req, res, next) => {
   const sql = 'INSERT INTO tasks (title, description, image, image_public_id, priority) VALUES (?, ?, ?, ?, ?)';
   try {
     const { imageUrl, public_id } = await uploadImage(req.body.image);
@@ -48,30 +55,28 @@ app.post('/tasks', async (req, res) => {
       req.body.priority
     ];
     await query(sql, values);
-    res.json("Task added succesfully!");
+    res.status(201).json("Task added successfully!");
   } catch (error) {
-    console.error('Error creating task:', error);
-    res.status(500).json({ message: 'Internal server error' });
+    next(error);
   }
 });
 
 //delete task
-app.delete('/tasks/:id', async (req, res) => {
+app.delete('/tasks/:id', async (req, res, next) => {
   const id = req.params.id;
   try {
     const image_id = await query('SELECT image_public_id FROM tasks WHERE id = ?', [id])
     await deleteImage(image_id[0].image_public_id);
     await query('DELETE FROM tasks WHERE id = ?', [id]);
-    res.json("Task deleted succesfully!");
+    res.status(204).json("Task deleted succesfully!");
   } catch (error) {
-    console.error('Error deleting task:', error);
-    res.status(500).json({ message: 'Internal server error' });
+    next(error);
   }
 });
 
 //update task
-app.put('/tasks/:id', async (req, res) => {
-  id = req.params.id;
+app.put('/tasks/:id', async (req, res, next) => {
+  const id = req.params.id;
   const sql = 'UPDATE tasks SET `title` = ?, `description` = ?, `image` = ?, `image_public_id` = ?, `priority` = ? WHERE id = ?';
   
   try {
@@ -87,10 +92,9 @@ app.put('/tasks/:id', async (req, res) => {
     ];
     await query(sql, values);
     await deleteImage(image_id[0].image_public_id);
-    res.json("Task updated succesfully!");
+    res.status(200).json("Task updated succesfully!");
   } catch (error) {
-    console.error('Error updating task:', error);
-    res.status(500).json({ message: 'Internal server error' });
+    next(error);
   }
 });
 
