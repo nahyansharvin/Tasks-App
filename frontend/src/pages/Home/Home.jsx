861a3a9c269b46b0c08c68011c3eab88c39702bc
Nahyan Sharvin <nskonnalath@gmail.com>
2023-10-19 23:49:19 +0530

    feat: filter tasks by priority

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -2,20 +2,40 @@ import React from 'react'
 import useHome from './useHome';
 import TaskCard from './components/TaskCard';
 import ContentWrapper from '../../components/common/Wrapper/ContentWrapper';
+import { useNavigate } from 'react-router';
 
 import Button from '@mui/material/Button';
 import AddIcon from '@mui/icons-material/Add';
-import { useNavigate } from 'react-router';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 
 const Home = () => {
   const navigate = useNavigate();
-  const { tasks, handleDelete } = useHome();
-  
+  const { tasks, handleDelete, filter, setFilter } = useHome();
+
   return (
     <ContentWrapper>
       <h1 className='text-3xl text-primary font-bold'>Tasks to be accomplished</h1>
       <div className='flex justify-between items-center my-4'>
-        <p>Sort by</p>
+        <div className='flex items-center'>
+          <p className='text-gray-500 mr-1'>Filter by</p>
+          <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+            <InputLabel id="demo-select-small-label">Priority</InputLabel>
+            <Select
+              labelId="demo-select-small-label"
+              value={filter}
+              label="Priority"
+              onChange={(e) => setFilter(e.target.value)}
+            >
+              <MenuItem value="none">None</MenuItem>
+              <MenuItem value="low">Low</MenuItem>
+              <MenuItem value="medium">Medium</MenuItem>
+              <MenuItem value="high">High</MenuItem>
+            </Select>
+          </FormControl>
+        </div>
         <Button variant="contained" endIcon={<AddIcon />} onClick={() => navigate("/add")}>
           Add Task
         </Button>
--- frontend/src/pages/Home/useHome.js
+++ frontend/src/pages/Home/useHome.js
@@ -5,10 +5,14 @@ import { Success } from '../../components/common/Toast';
 
 const useHome = () => {
     const [tasks, setTasks] = useState();
+    const [filteredTasks, setFilteredTasks] = useState();
+    const [filter, setFilter] = useState('none');
+
     async function fetchTasks() {
         try {
             const tasks = await getTasks();
             setTasks(tasks);
+            setFilteredTasks(tasks);
         } catch (err) {
             handleApiError(err);
         }
@@ -24,11 +28,21 @@ const useHome = () => {
         }
     }
 
+    //Filter tasks by priority
+    useEffect(() => {
+        if (filter === 'none') {
+            setFilteredTasks(tasks);
+        } else {
+            const filteredTasks = tasks?.filter((task) => task.priority === filter);
+            setFilteredTasks(filteredTasks);
+        }
+    }, [filter]);
+
     useEffect(() => {
         fetchTasks();
     }, []);
 
-    return { tasks, handleDelete }
+    return { tasks:filteredTasks, handleDelete, filter, setFilter }
 }
 
 export default useHome
